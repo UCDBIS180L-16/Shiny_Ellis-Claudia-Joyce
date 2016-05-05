5de4e264ae84724da05504fc95b39babe1b7080e
Ellis-Anderson <eaanderson@ucdavis.edu>
2016-05-05 16:12:37 -0700

    Mostly working versions of the UI and server

--- server.R
+++ server.R
@@ -4,15 +4,15 @@ library(ggplot2)
 shinyServer(function(input, output) {
   
 
-  output$boxPlot <- renderPlot({
+  output$Plot <- renderPlot({
 
    
     pl <- ggplot(data = data.pheno.mds,
                  #Use aes_string so variables are interpreted correctly
                  
-                 aes_string(x= input$population, #allows for user to pick popID or region
-                            y= input$trait, #allows user to specify trait of interest
-                            fill= input$population #colors plots based on pop or region
+                 aes_string(x= input$split, #allows for user to pick popID or region
+                            y= input$traits, #allows user to specify trait of interest
+                            fill= input$split #colors plots based on pop or region
                  )
     )
     
--- ui.R
+++ ui.R
@@ -11,7 +11,7 @@ shinyUI(fluidPage(
   helpText("This application creates a plot of your choice for selected rice phenotypes",
            "based on ancestral population or region. Please use the radio box below to",
            "choose a) your type of plot, b) your phenotypes to display, and c) whether",
-           "you want to plot by ancestral population or region.")  
+           "you want to plot by ancestral population or region."),  
   
   #Sidebar with a radio box to input which type of plot will be displayed
   sidebarLayout(
@@ -32,12 +32,12 @@ shinyUI(fluidPage(
       
       radioButtons("split",
                    "How do you want to split the data:",
-                   c("Ancestral Population",
-                     "Region"))
-      ))
+                   c("popID",
+                     "Region")
+      )),
   
     # Show a plot of the generated distribution
-    mainPanel(plotOutput(input$plot)
+    mainPanel(plotOutput("Plot")
     )
   )
 ))
