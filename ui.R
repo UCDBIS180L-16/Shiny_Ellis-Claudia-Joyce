f0321ce11dc418c6b79facc0fa708e31f67006e0
bis180l_local <cdastmalchi@ucdavis.edu>
2016-05-05 15:51:41 -0700

    Second update

--- ui.R
+++ ui.R
@@ -11,7 +11,7 @@ shinyUI(fluidPage(
   helpText("This application creates a plot of your choice for selected rice phenotypes",
            "based on ancestral population or region. Please use the radio box below to",
            "choose a) your type of plot, b) your phenotypes to display, and c) whether",
-           "you want to plot by ancestral population or region.")
+           "you want to plot by ancestral population or region.")  
   
   #Sidebar with a radio box to input which type of plot will be displayed
   sidebarLayout(
@@ -21,9 +21,23 @@ shinyUI(fluidPage(
                    c("Histogram",
                      "Violin Plot",
                      "Boxplot",
-                     "Scatter Plot")
-      )), ""
-    
+                     "Scatter Plot")),
+
+      radioButtons("traits",
+                   "Choose the phenotypes you would like to plot:",
+                   c("Protein.content",
+                     "Amylose.content",
+                     "Seed.volume",
+                     "Seed.length",
+                     "Plant.height",
+                     "Seed.Width")),
+      
+      radioButtons("split",
+                   "How do you want to split the data:",
+                   c("Ancestral Population",
+                     "Region"))
+      ))
+  
     # Show a plot of the generated distribution
     mainPanel(plotOutput(input$plot)
     )
